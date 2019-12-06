5e690cfef58c0501b863a6dbcbbae97dd81b66ff
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-06 20:09:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+require 'pry'
 
 def directors_totals(source)
   result = {}
@@ -25,19 +26,28 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  array = []
+  i = 0 
+  while i < source.length do 
+    array << source[i][:name]
+    i += 1 
+  end
+  return array
 end
 
 def total_gross(source)
-  # Write this implementation
+ grand_total = 0 
+ directors_totals
+ list_of_directors
+ return grand_total 
+end
+
+# Write this implementation
   #
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
   #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
+  # Visit each key (i.e. director name), look up the value in the hash returned by directors_totals, and add it to a running total. When done,
   # return the total
-end
-
 
